b7f774334e9a3fc442d8a6e605b65609db0ed628
ltaylanozgur <ltaylanozgur@gmail.com>
2022-09-20 23:18:41 +0300

    Ozgur_sql_lab_subqueries

--- sql_lab_subqueries.sql
+++ sql_lab_subqueries.sql
@@ -1,7 +1,7 @@
 USE sakila;
 
 -- 1. How many copies of the film Hunchback Impossible exist in the inventory system?
-SELECT COUNT(inventory_id)
+SELECT COUNT(inventory_id) AS 'amount of copies'
 FROM inventory
 WHERE film_id IN   
 	(SELECT film_id FROM film
@@ -42,12 +42,12 @@ WHERE film_id in
 -- that will help you get the relevant information.
 SELECT first_name, last_name, email FROM customer
 WHERE address_id IN (
-SELECT address_id FROM address
-WHERE city_id in
-	(SELECT city_id FROM city
-	WHERE country_id in
-		(SELECT country_id FROM country
-		WHERE country = 'Canada')));
+	SELECT address_id FROM address
+	WHERE city_id in
+		(SELECT city_id FROM city
+		WHERE country_id in
+			(SELECT country_id FROM country
+			WHERE country = 'Canada')));
 
 SELECT c.first_name, c.last_name, c.email FROM customer c
 JOIN address a
@@ -75,6 +75,17 @@ WHERE film_id IN (
 SELECT film_id FROM film_actor 
 WHERE actor_id = '107');
 
+SELECT film_id, title
+FROM film_actor
+LEFT JOIN film USING (film_id)
+WHERE actor_id = (SELECT actor_id
+					FROM(SELECT actor_id, COUNT(film_id)
+							FROM film_actor
+							GROUP BY actor_id
+							ORDER BY COUNT(film_id) DESC
+							LIMIT 1) sub1); -- can't have a limit at the end of subquery, have
+									-- to add another layer
+
 
 -- 7. Films rented by most profitable customer. You can use the customer table and payment table to find the most profitable customer ie the customer that has made the largest sum of payments
 SELECT customer_id, SUM(amount)
@@ -90,6 +101,17 @@ WHERE film_id IN (
 		SELECT inventory_id FROM rental 
 		WHERE customer_id = '526'));
 
+SELECT title
+FROM sakila.film
+WHERE film_id IN(
+	SELECT film_id FROM( 
+		SELECT film_id FROM sakila.inventory i
+		JOIN sakila.rental r USING(inventory_id)
+		WHERE customer_id = (SELECT customer_id FROM (
+			SELECT customer_id, SUM(amount) FROM sakila.payment
+			GROUP BY customer_id
+			ORDER BY SUM(amount) DESC
+			LIMIT 1) sub1)) sub2);
 
 -- 8. Customers who spent more than the average payments.
 
